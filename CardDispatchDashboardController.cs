4974f0ef8f1e85b6ce3f35682ccce2b6b39b56e9
Brevon10 <116813848+Brevon10@users.noreply.github.com>
2025-12-27 14:15:14 +0530

    Add files via upload

---
+++ CardDispatchDashboardController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CardDispatch.Models;
+using System;
+
+namespace CardDispatch.Controllers
+{
+    public class CardDispatchDashboardController : Controller
+    {
+        public IActionResult CardDispatchDashboard()
+        {
+            var model = new CardDispatchModel
+            {
+                FromDate = DateTime.Parse("2024-08-02"),
+                ToDate = DateTime.Parse("2024-08-02")
+            };
+            return View("~/Views/Dashboard/CardDispatchDashboard.cshtml", model);
+        }
+    }
+}
